58428384c8c3487023a19c699e620b8f37c345b4
tuckerferguson <tuckerferguson@u.boisestate.edu>
2017-09-20 16:01:03 -0600

    taco

--- backpack/CS253-1-f17/p2/main.c
+++ backpack/CS253-1-f17/p2/main.c
@@ -1,11 +1,58 @@
 #include <stdio.h>
 
 int main(int argc, char **argv)
+
 {
-  /*
-    Put your code here. You should create as many functions 
-    as necessary to create a modular program. We should not 
-    see ALL your code in the main function! 
-  */
-  return 1;
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int argc, char **argv)
+{
+  int mflag = 0;
+  int hflag = 0;
+  char *cvalue = NULL;
+  int index;
+  int c;
+
+  opterr = 0;
+
+  while ((c = getopt (argc, argv, "abc:")) != -1)
+    switch (c)
+      {
+      case 'a':
+        aflag = 1;
+        break;
+      case 'b':
+        bflag = 1;
+        break;
+      case 'c':
+        cvalue = optarg;
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+  printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+          aflag, bflag, cvalue);
+
+  for (index = optind; index < argc; index++)
+    printf ("Non-option argument %s\n", argv[index]);
+  return 0;
+}
+
+	return 1;
 }
