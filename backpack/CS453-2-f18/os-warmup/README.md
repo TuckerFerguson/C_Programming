5738bb7faf60eed5975de49269287ab7b2196542
Tucker <tuckerferguson@u.boisestate.edu>
2018-08-31 14:52:05 -0600

    Finished project

--- backpack/CS453-2-f18/os-warmup/README.md
+++ backpack/CS453-2-f18/os-warmup/README.md
@@ -1,7 +1,17 @@
-
 ###OS Warmup Project
 
-Update this file after finishing the os-warmup project.
+Author: Tucker Ferguson
+Class: CS453 Section 2
+
+Overview: This program parses output from the command ps augx continually until specified to stop. The desired piece of output being searched for is information regarding threading, specifically which tasks are multi-threaded.
+
+Compiling and Using: To compile one should use the command "make" which due to our makefile handles compiling of files and creates up to date executable. After which the user should follow the command line arguments "ps-logger <sleep interval>".This will begin the process of showing multi verse non-multi threaded processes. Since the program functions on a continuous loop the user must use Ctrl-C to stop when desired.
+
+Testing: Testing here was done with nested print statements ensuring that my program was parsing the correct information. Next I made sure values being calculated using the parsed information were correct by opening new processes on my node and ensuring my results would adjust accordingly.
+
+Sources Used: Included backpack files as well as old programs from previous course CS-253 were taken advantage of here as well as piazza.
+
+Known Issues: When compiled my program still has several warnings most of which are syntax specific. Inbetween the time I write this and submit the actual project hopefully these are all fixed.
 
 README.md: this file
 ps-sample.log: A sample output for "ps augx" command
--- backpack/CS453-2-f18/os-warmup/ps-logger.c
+++ backpack/CS453-2-f18/os-warmup/ps-logger.c
@@ -2,18 +2,65 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
 #define MAX_LENGTH 2048
 #define MAX_TOKENS 100
 
-int main(int argc, char **argv) 
-{
-	while (1) {
-		fprintf(stderr, "%s: running ps augx command.\n", argv[0]);
-		system("ps augx > ps.log");
+//author: Tucker Ferguson
 
-		sleep(2);
+int multiT, totalT;
+double percent;
+void warning1(void);
+void usage1(void);
+int main(int argc, char **argv){
+//Ensures that user enters proper command line arguments
+	if (argc != 2){
+		usage1();
 	}
-	exit(0);
+//Ensures that the time interval is greater than or equal to one
+	else if(atoi(argv[1]) < 1){
+		warning1();
+	}
+	else{
+	fprintf(stderr,"%s: Using a sleep time of %s seconds.\n", argv[0], argv[1]);
+	while(1){
+	multiT = 0;
+	totalT = 0;
+	percent = 0;
+	system("ps augx > ps.log");
+
+        FILE *fPoint;
+	if((fPoint = fopen("ps.log", "r")) == NULL){
+	 printf("File not found");
+	 exit(1);
+	}
+	char temp[MAX_LENGTH];
+//This scans the log file for strings "R...l" and "S...l"
+	while(fscanf(fPoint, " %1000s", temp) == 1){
+		if(temp[0] == 'R' && temp[strlen(temp) - 1] == 'l'){
+		multiT++;
+		}else if(temp[0]  == 'S' && temp[strlen(temp) - 1] == 'l'){
+		multiT++;
+		}
+	}
+//Points back to start of file input string for scanner
+	rewind(fPoint);
+//Gets value of total processes
+	while(fgets(temp,30000,fPoint)!= NULL){
+		totalT++;
+	}
+	percent = 100.0 * multiT / totalT;
+	fprintf(stdout, "%s: %d/%d processes are multithreaded. [%.2f%c]\n",argv[0],multiT,totalT,percent,'%');
+		sleep(atoi(argv[1]));
+	}
+		exit(0);
+	}
+		return 0;
+}
+void usage1(){
+        printf("Usage: ps-logger <sleep interval(seconds)>\n");
+}
+
+void warning1(){
+	printf("ps-logger:Specified too small an interval. Must be at least 1 second.\n");
 }
 
