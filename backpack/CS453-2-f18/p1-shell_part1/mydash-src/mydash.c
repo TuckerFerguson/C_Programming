cb12161ee5d1bf56e18e337a519b6afc8f59795a
Tucker <tuckerferguson@u.boisestate.edu>
2018-09-12 15:42:01 -0600

    test

--- backpack/CS453-2-f18/p1-shell_part1/mydash-src/mydash.c
+++ backpack/CS453-2-f18/p1-shell_part1/mydash-src/mydash.c
@@ -1,12 +1,42 @@
 /*
   This program needs the file error.c and mydash.h to compile.
  */
+#include "error.c"
+#include "mydash.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include    <sys/types.h>
 #include    <sys/wait.h>
 #include    <mydash.h>
 
+/*Project Specs*/
+#define MAXARGS 2048
+#define MAXLINE 4096
+
+
+//#added methods
+char** chop_line_exec(const char *);
+char* strncpy_safe(char *dst, const char *src, size_t len);
+void emptyCommand(char*);
+
+
 int main(void)
 {
+/*#### ADDED CODE ###
+    char *line;
+	char *prompt = "mydash>";
+	using_history();
+	while ((line=readline(prompt)))
+			{
+					printf("%s\n",line);
+					add_history(line);
+					free(line);
+		    }
+### END OF ADDED CODE####*/
+
     char    buf[MAXLINE];
     pid_t   pid;
     int     status;
@@ -24,6 +54,12 @@ int main(void)
             exit(EXIT_FAILURE);
         }
 
+/*###### ADDED CODE ######*/
+		else if (pid == -1){
+		fprintf(stderr,"Error, fork did not work");
+		}
+/*##### ADDED CODE ENDED ####*/
+
         /* parent */
         if ( (pid = waitpid(pid, &status, 0)) < 0)
             err_sys("waitpid error");
@@ -32,4 +68,24 @@ int main(void)
     exit(EXIT_SUCCESS);
 }
 
+/*#### NEW METHOD FOR TOKENIZING##
+char** chop_line_exec(const char* line)
+{
+		char *token = NULL;
+		int i = 0;
+		char linecpy[MAXLINE];
+		char **rval = malloc(MAXARGS);
+		memset(rval,0,MAXARGS);
+		strncpy_safe(linecpy,line,MAXLINE);
+
+		token = strtok(linecpy," ");
+		for(i=0; token && i<MAXARGS;i++){
+				//fprintf(stderr,"got token %s\n",token);
+				rval[i]=malloc(MAXLINE);
+				strncpy_safe(rval[i],token,MAXLINE);
+				token=strtok(NULL," ");
+		}
+		return rval;
+}
+*/
 /* vim: set ts=4: */
