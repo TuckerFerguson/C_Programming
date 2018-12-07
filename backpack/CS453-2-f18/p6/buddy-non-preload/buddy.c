e8152a6edca6e07746b5b26121a71991878acd26
Tucker <tuckerferguson@u.boisestate.edu>
2018-12-07 15:49:27 -0700

    finished project

--- backpack/CS453-2-f18/p6/buddy-non-preload/buddy.c
+++ backpack/CS453-2-f18/p6/buddy-non-preload/buddy.c
@@ -64,7 +64,7 @@ int buddy_init(size_t size) {
         pool.start = sbrk(pool.size);
     }
     if(size < DEFAULT_MAX_MEM_SIZE && size >= 1){
-        pool.size = size;
+        pool.size = DEFAULT_MAX_MEM_SIZE;
 //make sure to change from int to long int        
         pool.start = sbrk(1L << pool.size);
         pool.start = sbrk(pool.size);
@@ -103,9 +103,27 @@ int buddy_init(size_t size) {
 
 void *buddy_malloc(size_t size)
 {
-    // unsigned int bytes_toAlloc;
-    // buddy_init(size);
-    // bytes_toAlloc = ((size+sizeof(block_header)-1)/sizeof(block_header))+ 1);
+//         if (argc != 2)
+//         {
+//                 fprintf(stderr, "Usage: %s <chunk size> \n", argv[0]);
+//                 exit(1);
+//         }
+//         chunksize = atoi(argv[1]);
+
+//         prev_endptr = sbrk(0); // argument of zero returns end of data segment
+
+//         for (i = 0; i <1000; i++)
+//         {
+//                 ptr = (char *) malloc(sizeof(char)*chunksize);
+//                 memset(ptr, 0, chunksize*sizeof(char));
+//                 new_endptr = sbrk(0); // argument of zero returns end of data segment
+//                 if (new_endptr != prev_endptr)
+//                 {
+//                         printf("iteration %d: end of data segment = %lx \n", i, (unsigned long)new_endptr);
+//                         prev_endptr = new_endptr;
+//                 }
+//         }
+
      return NULL;
 
 }
@@ -141,9 +159,9 @@ void printBuddyLists()
     for (i = 0; i < MAX_KVAL; i++) {
         printf("List %d: head: %p", i, &pool.avail[i]);
         if(pool.avail[i].tag == UNUSED){
-            printf("NULL\n");
+            printf("--> <null>\n");
         } else {
-            printf("[tag: %d, kval: %d, addr: %p]\n", pool.avail[i].next->tag, pool.avail[i].next->kval, &pool.avail[i].next);
+            printf(" --> [tag=%d, kval=%d, addr=%p]\n", pool.avail[i].next->tag, pool.avail[i].next->kval, &pool.avail[i].next);
         }
     }
 }
